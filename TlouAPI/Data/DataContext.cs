921520847c830b62aa94adc0995d5f75219b2393
Rafael Santos <rafa.05125@gmail.com>
2024-03-27 10:25:35 -0300

    Initial migration

--- TlouAPI/Data/DataContext.cs
+++ TlouAPI/Data/DataContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TlouAPI.Models;
 
 namespace TlouAPI.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Backpack> Backpacks { get; set; }
     }
 }
