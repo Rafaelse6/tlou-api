fe92f92586a69487ce4d6bca372b4d72a3ada1dc
Rafael Santos <rafa.05125@gmail.com>
2024-03-27 10:32:07 -0300

    CharacterWeaponsMigration, 1:n relationship

--- TlouAPI/Data/DataContext.cs
+++ TlouAPI/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace TlouAPI.Data
     
         public DbSet<Character> Characters { get; set; }
         public DbSet<Backpack> Backpacks { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
     }
 }
