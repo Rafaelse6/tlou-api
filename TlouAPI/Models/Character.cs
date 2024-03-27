7d7a145428955b4a211a627cabbdb4023e588a1b
Rafael Santos <rafa.05125@gmail.com>
2024-03-27 10:25:18 -0300

    Character, Backpack, 1:1 relationship

---
+++ TlouAPI/Models/Character.cs
@@ -0,0 +1,9 @@
+﻿namespace TlouAPI.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Backpack Backpack { get; set; }
+    }
+}
